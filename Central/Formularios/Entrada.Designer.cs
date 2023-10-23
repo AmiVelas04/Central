db0e46f706a96c58a9b811e70c611d6c276bb2f1
Dev 11 <correo>
2023-10-23 16:30:18 -0600

    Inicio de Entradas

---
+++ Central/Formularios/Entrada.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Central.Formularios
+{
+    partial class Entrada
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PanSup = new System.Windows.Forms.Panel();
+            this.PanCent = new System.Windows.Forms.Panel();
+            this.Dgv1 = new System.Windows.Forms.DataGridView();
+            this.Codigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Desc = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Marca = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Costo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PrecioV = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PrecioV2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PrecioV3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Cant = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PanCent.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PanSup
+            // 
+            this.PanSup.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PanSup.Location = new System.Drawing.Point(0, 0);
+            this.PanSup.Name = "PanSup";
+            this.PanSup.Size = new System.Drawing.Size(582, 107);
+            this.PanSup.TabIndex = 0;
+            // 
+            // PanCent
+            // 
+            this.PanCent.Controls.Add(this.Dgv1);
+            this.PanCent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PanCent.Location = new System.Drawing.Point(0, 107);
+            this.PanCent.Name = "PanCent";
+            this.PanCent.Size = new System.Drawing.Size(582, 260);
+            this.PanCent.TabIndex = 1;
+            // 
+            // Dgv1
+            // 
+            this.Dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Dgv1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Codigo,
+            this.Nombre,
+            this.Desc,
+            this.Marca,
+            this.Costo,
+            this.PrecioV,
+            this.PrecioV2,
+            this.PrecioV3,
+            this.Cant});
+            this.Dgv1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Dgv1.Location = new System.Drawing.Point(0, 0);
+            this.Dgv1.Name = "Dgv1";
+            this.Dgv1.Size = new System.Drawing.Size(582, 260);
+            this.Dgv1.TabIndex = 0;
+            // 
+            // Codigo
+            // 
+            this.Codigo.HeaderText = "Codigo";
+            this.Codigo.Name = "Codigo";
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            // 
+            // Desc
+            // 
+            this.Desc.HeaderText = "Descripcion";
+            this.Desc.Name = "Desc";
+            // 
+            // Marca
+            // 
+            this.Marca.HeaderText = "Marca";
+            this.Marca.Name = "Marca";
+            // 
+            // Costo
+            // 
+            this.Costo.HeaderText = "Costo";
+            this.Costo.Name = "Costo";
+            // 
+            // PrecioV
+            // 
+            this.PrecioV.HeaderText = "PrecioV1";
+            this.PrecioV.Name = "PrecioV";
+            // 
+            // PrecioV2
+            // 
+            this.PrecioV2.HeaderText = "PrecioV2";
+            this.PrecioV2.Name = "PrecioV2";
+            // 
+            // PrecioV3
+            // 
+            this.PrecioV3.HeaderText = "PrecioV3";
+            this.PrecioV3.Name = "PrecioV3";
+            // 
+            // Cant
+            // 
+            this.Cant.HeaderText = "Cantidad";
+            this.Cant.Name = "Cant";
+            // 
+            // Entrada
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 367);
+            this.Controls.Add(this.PanCent);
+            this.Controls.Add(this.PanSup);
+            this.Name = "Entrada";
+            this.Text = "Entrada";
+            this.PanCent.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PanSup;
+        private System.Windows.Forms.Panel PanCent;
+        private System.Windows.Forms.DataGridView Dgv1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Codigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Desc;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Marca;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Costo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PrecioV;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PrecioV2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PrecioV3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cant;
+    }
+}
